d8308a6b296098450ebe6ed885d1d4a418d6ee29
Phospharadscha <95589284+Phospharadscha@users.noreply.github.com>
2022-09-29 11:15:17 +0100

    Commented authentication.py
    Added comments to explain the authentication blueprint

--- flaskr/authentication.py
+++ flaskr/authentication.py
@@ -15,31 +15,53 @@ url_prefix is put at the start of all urls associated with authentication
 """
 blueprint = Blueprint('auth', __name__, url_prefix='/auth')
 
-@blueprint.route('/register', methods=('GET', 'POST'))
+@blueprint.route('/register', methods=('GET', 'POST'))   # Associates the '/register/ url with the register view function
 def register():
-    if request.method == 'POST':
+    """When the user visits '/auth/register' the register returns a HTML with form for registration.
+    When a form is submitted, it will validate input and either show the form again, or create a new user.
+    This does not include the HMTL template. 
+    """
+    
+    if request.method == 'POST':   # If user submitted the form
+        # request.form[] is a type of dictionary mapping. 
         username = request.form['username']
         password = request.form['password']
+        
+        # Retrieve the database for storing user information
         database = get_database()
+        
+        # Will store any error from the result of input validation
         error = None
 
+        # Validate user input.
+        # Check that they are not empty
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
 
+        # If validation was successful, insert their data into the user table
         if error is None:
+            
+            # Try catch for error handling
             try:
-                database.execute(
+                # takes SQL query 
+                # ? are placeholders for user input, tuple is what to replace the placeholders with
+                # The database library will automatically protect from SQL injection 
+                database.execute( 
                     "INSERT INTO user (username, password) VALUES (?, ?)",
                     (username, generate_password_hash(password)),
                 )
-                database.commit()
-            except database.IntegrityError:
+                database.commit() # Commit changes to the database
+            except database.IntegrityError: # If username is already in use
                 error = f"User {username} is already registered."
-            else:
+            else: # If no exception, then redirect to login page
+                # url_for automatially generates the url for login based on name
+                # redirect then redirects the user to that url
                 return redirect(url_for("auth.login"))
 
+        # Flash stores messages that can be retrieved when rendering the template
         flash(error)
 
+    # renders a template containing the HTML
     return render_template('auth/register.html')
