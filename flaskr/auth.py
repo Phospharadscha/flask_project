c718b93af176b9683de120c4e2ce57f1f13ad23c
Phospharadscha <95589284+Phospharadscha@users.noreply.github.com>
2022-09-29 10:56:51 +0100

    Commented auth.py
    Added comments to the authentication blueprint generation module.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,11 +1,11 @@
 import functools # Higher order functions and operations on callable objects
+from werkzeug.security import check_password_hash, generate_password_hash # Security related functions
+from flaskr.db import get_db # Import the function to get a connection to the database
 
+# A blueprint is a way to organize a group of related views, or other code.
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from werkzeug.security import check_password_hash, generate_password_hash
-
-from flaskr.db import get_db
 
 """Creates a Flask blueprint named 'auth'. 
 This will be used for authentication of users. 
